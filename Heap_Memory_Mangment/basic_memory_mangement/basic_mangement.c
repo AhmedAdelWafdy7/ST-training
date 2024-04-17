b3da0764b5e4fb9915642404f22cdf30fbce2389
AhmedAdelWafdy7 <fandes99@gmail.com>
2024-04-17 18:02:42 +0200

    replacing libc

--- Heap_Memory_Mangment/basic_memory_mangement/basic_mangement.c
+++ Heap_Memory_Mangment/basic_memory_mangement/basic_mangement.c
@@ -16,7 +16,7 @@ header_t *search_freeblock(size_t size){
 
 
 //The malloc(size) function allocates size bytes of memory and returns a pointer to the allocated memory.
-void *mymalloc(size_t size)
+void *malloc(size_t size)
 {
 //	printf("Hello from wafdunix malloc implemntation\n");
 	size_t total_size;
@@ -64,7 +64,7 @@ void *mymalloc(size_t size)
 
 
 //free() has to first determine if the block-to-be-freed is at the end of the heap if it is ,we can release it
-void myfree(void *block)
+void free(void *block)
 {
 //	printf("\nHello from wafdunix free implemntation");
 	header_t *header, *tmp;
@@ -104,8 +104,8 @@ void myfree(void *block)
 
 
 
-/*
- A debug function to print the entire link list 
+
+// A debug function to print the entire link list 
 void print_mem_list()
 {
 	header_t *curr = head;
@@ -116,7 +116,7 @@ void print_mem_list()
 		curr = curr->s.next;
 	}
 }
-*/
+
 //the calloc fn. allocates memory for an array of num elements of numsize bytes each and initialise it to zero , returns pointer for the allocated memory.
 
 void *calloc(size_t num, size_t nsize){
@@ -129,7 +129,7 @@ void *calloc(size_t num, size_t nsize){
         size = num * nsize;
         if(nsize != size /num)  // check for multiplicative overflow
                 return NULL;
-        block = mymalloc(size);
+        block = malloc(size);
         if(!block)
                 return NULL;
         memset(block,0,size);
@@ -144,12 +144,12 @@ void *realloc(void *block, size_t size){
 	void *ret;
 	// if the block has already the requeted size ther's nothing to be done 
 	if(!block || !size)
-	       return mymalloc(size);
+	       return malloc(size);
 	header = (header_t*)block - 1;
 	// if the current block doesn't have the requested size , malloc can get a block of the requested size 
 	if (header->s.size >= size)
 		return block;
-	ret = mymalloc(size);
+	ret = malloc(size);
 	//relocate contents to the new bigger block using memcpy() , freeing the old block
 	if(ret){
 		memcpy(ret,block,header->s.size);
